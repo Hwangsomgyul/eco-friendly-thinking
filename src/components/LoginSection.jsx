ecd0743dc98b5e1ae342e4d3b32f933f1bed91c8
[AI11] 김민지 <kkimming__@naver.com>
2024-08-03 00:30:35 +0900

    feat : login callback

--- src/components/LoginSection.jsx
+++ src/components/LoginSection.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logo from "../asserts/logo.png";
-import kakaoLogin from "../asserts/kakao_login_medium.png";
+// import logo from "../../public/images/";
+// import kakaoLogin from "../../public/kakao_login_medium.png";
 
 const handleLogin = () => {
   const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}&response_type=code`;
@@ -12,11 +12,15 @@ export const LoginSection = () => {
     <>
       <div className="w-1/5 pt-300 flex flex-col gap-30">
         <div className="flex w-3/5">
-          <img src={logo} alt="Logo" />
+          <img src="/logo.png" alt="Logo" />
         </div>
         <div className="flex">
           <div className="flex pl-20 w-1/2">
-            <img src={kakaoLogin} alt="Kakao Login" onClick={handleLogin} />
+            <img
+              src="/kakaoLogin.png"
+              alt="Kakao Login"
+              onClick={handleLogin}
+            />
           </div>
         </div>
       </div>
