37e561d7b0528b1bc8fc26ea72a1a3e3ab3ced06
[AI11] 김민지 <kkimming__@naver.com>
2024-08-05 01:33:57 +0900

    feat : 모달 팝업 작업 중

--- src/components/ChartSection.jsx
+++ src/components/ChartSection.jsx
@@ -1,26 +1,23 @@
-import React from "react";
-import { WasteWithPop } from "./WasteWithPop";
+import React, { useState } from "react";
+import { WasteWithPop } from "../asserts/Chart/WasteWithPop";
 import "./ChartSection.css";
 
-export const ChartSection = ({ num, text }) => {
+export const ChartSection = ({ num, text, onClick }) => {
   return (
-    <>
-      {/* <div id="slide" className="w-1/5"></div> */}
-      <article className="w-1/5">
-        <div className="pt-100 w-full h-full inner" id="chartSection">
-          <div className="h-full flex flex-col justify-around p-5">
-            <div>
-              <div className="border-b-2 border-lightGreen w-1/5">
-                <h2 className="text-gray-300 text">{num}</h2>
-              </div>
-              <p className="pt-8 text-gray-400">{text}</p>
+    <article className="w-1/5" onClick={onClick}>
+      <div className="pt-100 w-full h-full inner" id="chartSection">
+        <div className="h-full flex flex-col justify-around p-5">
+          <div>
+            <div className="border-b-2 border-lightGreen w-1/5">
+              <h2 className="text-gray-300">{num}</h2>
             </div>
-            <div className="bg-white chart">
-              <p>simple 차트 삽입</p>
-            </div>
+            <p className="pt-8 text-gray-400">{text}</p>
+          </div>
+          <div className="bg-white chart">
+            <p>simple 차트 삽입</p>
           </div>
         </div>
-      </article>
-    </>
+      </div>
+    </article>
   );
 };
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,28 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { LoginSection } from "../components/LoginSection";
 import { ChartSection } from "../components/ChartSection";
+import { ChartModal01 } from "../components/ChartModal/ChartModal01";
 
 export const Login = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+
   return (
     <main className="w-full h-full bg-blackGreen">
       <div className="flex h-full gap-8 mx-12">
         <LoginSection></LoginSection>
-        <ChartSection num={"01"} text={"차트설명"}></ChartSection>
-        <ChartSection num={"02"} text={"차트설명"}></ChartSection>
-        <ChartSection num={"03"} text={"차트설명"}></ChartSection>
-        <ChartSection num={"04"} text={"차트설명"}></ChartSection>
+        <ChartSection
+          num={"01"}
+          text={"차트설명"}
+          onClick={openModal}
+        ></ChartSection>
+        <ChartSection
+          num={"02"}
+          text={"차트설명"}
+          onClick={openModal}
+        ></ChartSection>
+        <ChartSection
+          num={"03"}
+          text={"차트설명"}
+          onClick={openModal}
+        ></ChartSection>
+        <ChartSection
+          num={"04"}
+          text={"차트설명"}
+          onClick={openModal}
+        ></ChartSection>
       </div>
 
-      {/* <aside>
-        <div class="txt">
-          <h1>01</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-          <span>CLOSE</span>
-        </div>
-        <figure>
-          <video src="vids/vid1.mp4" loop muted></video>
-        </figure>
-      </aside> */}
+      <ChartModal01 isOpen={isModalOpen}></ChartModal01>
     </main>
   );
 };
