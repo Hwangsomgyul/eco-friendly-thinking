c4b8282518ed268a8710c310ece5d51985144f39
moonformeli <mgyang95@gmail.com>
2024-08-05 23:15:34 +0900

    refactor: NewKakao 컴포넌트 간단하게만 코드 정리

--- src/pages/NewKakao.jsx
+++ src/pages/NewKakao.jsx
@@ -8,6 +8,10 @@ export default function NewKakao() {
   const [map, setMap] = useState(null);
   const [info, setInfo] = useState(null);
 
+  const handleCreateMap = (map) => {
+    setMap(map);
+  };
+
   const handleChange = (e) => {
     setSearch(e.target.value);
   };
@@ -21,30 +25,36 @@ export default function NewKakao() {
     const ps = new kakao.maps.services.Places();
 
     ps.keywordSearch('이태원 맛집', (data, status, _pagination) => {
-      if (status === kakao.maps.services.Status.OK) {
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
-        // LatLngBounds 객체에 좌표를 추가합니다
-        const bounds = new kakao.maps.LatLngBounds();
-        let markers = [];
-
-        for (var i = 0; i < data.length; i++) {
-          // @ts-ignore
-          markers.push({
-            position: {
-              lat: data[i].y,
-              lng: data[i].x,
-            },
-            content: data[i].place_name,
-          });
-          // @ts-ignore
-          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
-        }
-        setMarkers(markers);
-
-        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
-        map.setBounds(bounds);
+      if (status !== kakao.maps.services.Status.OK) {
+        return;
       }
+
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+      // LatLngBounds 객체에 좌표를 추가합니다
+      const bounds = new kakao.maps.LatLngBounds();
+      const markers = [];
+
+      for (var i = 0; i < data.length; i++) {
+        // @ts-ignore
+        markers.push({
+          position: {
+            lat: data[i].y,
+            lng: data[i].x,
+          },
+          content: data[i].place_name,
+        });
+        // @ts-ignore
+        bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+      }
+      setMarkers(markers);
+
+      // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+      map.setBounds(bounds);
     });
+  };
+
+  const handleClickMarker = (marker) => () => {
+    setInfo(marker);
   };
 
   return (
@@ -71,15 +81,13 @@ export default function NewKakao() {
             lng: 126.975289,
           }}
           level={3}
-          onCreate={(map) => {
-            setMap(map);
-          }}
+          onCreate={handleCreateMap}
         >
           {markers.map((marker) => (
             <MapMarker
               key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
               position={marker.position}
-              onClick={() => setInfo(marker)}
+              onClick={handleClickMarker(marker)}
             >
               {info && info.content === marker.content && (
                 <div style={{ color: '#000' }}>{marker.content}</div>
