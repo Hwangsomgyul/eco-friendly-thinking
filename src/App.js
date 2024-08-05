b25b7e1f6ee61ae3bb6b08604d8eab1f27bb252a
[AI11] 김민지 <kkimming__@naver.com>
2024-08-05 14:44:57 +0900

    style : edit chart modal css

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import MainPage from "./components/MainPage";
+// import About from "./components/";
+// import ReviewPage from "./components/ReviewPage.jsx";
 import Pagination from "./components/Pagination";
 import MyPage from "./components/MyPage";
 import Forum from "./components/Forum";
@@ -11,27 +13,29 @@ import KakaoMap from "./components/KakaoMap";
 import Star from "./components/Star";
 
 import Modal from "./components/Modal";
-import ReviewModal from "./components/reviewModal";
-import PhotoReviewModal from "./components/PhotoReivewModal";
+import ReviewModal from "./components/ReviewModal.jsx";
+// import PhotoReviewModal from "./components/PhotoReviewModal.jsx";
 import ForumModal from "./components/ForumModal";
 
-import { Login } from "./pages/Login";
-import { RecycleRate } from "./asserts/Chart/RecycleRate";
+import { Login } from "./pages/Login.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/MainPage" element={<MainPage />} />
+      {/* <Route path="/About" element={<About />} /> */}
+      {/* <Route path="/ReviewPage" element={<ReviewPage />} /> */}
       <Route path="/MyPage" element={<MyPage />} />
       <Route path="/Pagination" element={<Pagination />} />
       <Route path="/Forum" element={<Forum />} />
-      <Route path="/Modal" element={<Modal />} />
+
       <Route path="/Star" element={<Star />} />
+      <Route path="/Modal" element={<Modal />} />
       <Route path="/ReviewModal" element={<ReviewModal />} />
-      <Route path="/PhotoReviewModal" element={<PhotoReviewModal />} />
+      {/* <Route path="/PhotoReviewModal" element={<PhotoReviewModal />} /> */}
       <Route path="/ForumModal" element={<ForumModal />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="/" element={<RecycleRate />} />
+
+      <Route path="/" element={<Login />} />
 
       <Route path="/KakaoMap" element={<KakaoMap />} />
     </Routes>
--- src/components/ChartModal/ChartModal01.jsx
+++ src/components/ChartModal/ChartModal01.jsx
@@ -11,16 +11,18 @@ export const ChartModal01 = ({ isOpen, onClose }) => {
     <aside className={`bg-blackGreen ${isOpen ? "on" : ""}`}>
       <div className="txt">
         <h1>01</h1>
-        <p>
-          서울시 생활폐기물은<br></br>최근 22년동안 매립양은 줄고,<br></br>
-          재활용량과 소각량이 상대적으로 증가했습니다.
-        </p>
-        <p>서울시 생활폐기물 관련 재활용률은 60%대 입니다.</p>
-        <p>더 높은 재활용률을 위해서 어떤 일을 할 수 있을까요?</p>
-        {/* <span onClick={closeModal}>CLOSE</span> */}
+        <span className="hover:text-white">CLOSE</span>
       </div>
       <figure>
         <WasteWithPop />
+        <div>
+          <p>
+            서울시 생활폐기물은 최근 22년동안 매립양은 줄고, 재활용량과 소각량이
+            상대적으로 증가했습니다.
+          </p>
+          <p>서울시 생활폐기물 관련 재활용률은 60%대 입니다.</p>
+          <p>더 높은 재활용률을 위해서 어떤 일을 할 수 있을까요?</p>
+        </div>
       </figure>
     </aside>
   );
