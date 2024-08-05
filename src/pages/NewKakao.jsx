e30cbc9489bd4154ab7523d760a07cd0734f51ec
moonformeli <mgyang95@gmail.com>
2024-08-05 23:12:46 +0900

    feat: 새로운 컴포넌트에서 지도 생성하고 검색 기능 부착

---
+++ src/pages/NewKakao.jsx
@@ -0,0 +1,93 @@
+/* eslint-disable no-undef */
+import { useState } from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+
+export default function NewKakao() {
+  const [search, setSearch] = useState('');
+  const [markers, setMarkers] = useState([]);
+  const [map, setMap] = useState(null);
+  const [info, setInfo] = useState(null);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const handleSearch = () => {
+    if (!search) {
+      return;
+    }
+
+    // eslint-disable-next-line no-undef
+    const ps = new kakao.maps.services.Places();
+
+    ps.keywordSearch('이태원 맛집', (data, status, _pagination) => {
+      if (status === kakao.maps.services.Status.OK) {
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정하기위해
+        // LatLngBounds 객체에 좌표를 추가합니다
+        const bounds = new kakao.maps.LatLngBounds();
+        let markers = [];
+
+        for (var i = 0; i < data.length; i++) {
+          // @ts-ignore
+          markers.push({
+            position: {
+              lat: data[i].y,
+              lng: data[i].x,
+            },
+            content: data[i].place_name,
+          });
+          // @ts-ignore
+          bounds.extend(new kakao.maps.LatLng(data[i].y, data[i].x));
+        }
+        setMarkers(markers);
+
+        // 검색된 장소 위치를 기준으로 지도 범위를 재설정합니다
+        map.setBounds(bounds);
+      }
+    });
+  };
+
+  return (
+    <section className='flex items-center justify-center'>
+      <div className='flex flex-col gap-4'>
+        <input
+          className='border-[1px] border-solid border-red-300'
+          value={search}
+          onChange={handleChange}
+        />
+        <button
+          className='border-[1px] border-solid border-red-300'
+          onClick={handleSearch}
+        >
+          검색
+        </button>
+      </div>
+      <section className='w-[980px] h-[740px]'>
+        <Map
+          style={{ width: '100%', height: '100%' }}
+          center={{
+            // 지도의 중심좌표
+            lat: 37.560159,
+            lng: 126.975289,
+          }}
+          level={3}
+          onCreate={(map) => {
+            setMap(map);
+          }}
+        >
+          {markers.map((marker) => (
+            <MapMarker
+              key={`marker-${marker.content}-${marker.position.lat},${marker.position.lng}`}
+              position={marker.position}
+              onClick={() => setInfo(marker)}
+            >
+              {info && info.content === marker.content && (
+                <div style={{ color: '#000' }}>{marker.content}</div>
+              )}
+            </MapMarker>
+          ))}
+        </Map>
+      </section>
+    </section>
+  );
+}
