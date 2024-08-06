1fd4ef404fe117400cf8e94a8a138771892bd2cd
[AI11] 김민지 <kkimming__@naver.com>
2024-08-06 19:21:25 +0900

    전체 삭제

--- src/pages/NotFound.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-export function NotFound() {
-  return <h1>404 Error</h1>;
-}
--- src/utils/changePx.js
+++
@@ -1,9 +0,0 @@
-const changePx = {};
-
-for (let i = 0; i <= 300; i++) {
-  if (i % 2 === 0) {
-    changePx[i] = `${i}px`;
-  }
-}
-
-export { changePx };
