e55f97c7f5917ee7a2289094d28b7bca29d2ba82
[AI11] 김민지 <kkimming__@naver.com>
2024-08-07 15:47:48 +0900

    fix : import 에러 해결

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,48 @@
-import React, { Component } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
+import React, { Component, useEffect } from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import './App.css';
 
-import { Login } from "./pages/Login.jsx";
-import KakaoCallback from "./service/KakaoCallback.js";
-import Header from "./components/Header.jsx";
-import MainPage from "./components/MainPage.jsx";
-import About from "./components/About.jsx";
-import ReviewPage from "./components/ReviewPage.jsx";
-import Pagination from "./components/Pagination.jsx";
-import MyPage from "./components/MyPage.jsx";
-import Forum from "./components/Forum.jsx";
-import KakaoMap from "./components/KakaoMap.jsx";
-import Star from "./components/Star.jsx";
-import Modal from "./components/Modal.jsx";
-import ReviewModal from "./components/reviewModal.jsx";
-import PhotoReviewModal from "./components/PhotoReviewModal.jsx";
-import ForumModal from "./components/ForumModal.jsx";
-import Store from "./pages/Store.jsx";
-import EventPage from "./pages/EventPage.jsx";
-import EventOver from "./pages/EventOver.jsx";
-import Footer from "./components/Footer.jsx";
+import { Login } from './pages/Login.jsx';
+import Header from './components/Header.jsx';
+import MainPage from './components/MainPage.jsx';
+import About from './components/About.jsx';
+import ReviewPage from './components/ReviewPage.jsx';
+
+import MyPage from './components/MyPage.jsx';
+import Forum from './components/Forum.jsx';
+
+import Store from './pages/Store.jsx';
+import EventPage from './pages/EventPage.jsx';
+import EventOver from './pages/EventOver.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
-  if (!localStorage.getItem("token")) {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const hash = window.location.hash;
+    const params = new URLSearchParams(hash.substring(1));
+    const accessToken = params.get('access_token');
+    const userId = params.get('user_id');
+    if (accessToken && userId) {
+      localStorage.setItem('accessToken', accessToken);
+      localStorage.setItem('userId', userId);
+      navigate('/MainPage');
+    }
+  }, [navigate]);
+
+  if (
+    !(localStorage.getItem('accessToken') && localStorage.getItem('userId'))
+  ) {
     return (
       <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="/auth/kakao/callback" element={<KakaoCallback />} />
 
         {/* 위에 없는 경로일 경우 올바르지 못한 접근 */}
         {/* <Route path="/*" element={<Unauthorized />} />  */}
       </Routes>
     );
   }
+
   return (
     <div className="app-container">
       <Header />
@@ -43,14 +52,8 @@ function App() {
           <Route path="/About" element={<About />} />
           <Route path="/ReviewPage" element={<ReviewPage />} />
           <Route path="/MyPage" element={<MyPage />} />
-          <Route path="/Pagination" element={<Pagination />} />
           <Route path="/Forum" element={<Forum />} />
-          <Route path="/Star" element={<Star />} />
-          <Route path="/Modal" element={<Modal />} />
-          <Route path="/ReviewModal" element={<ReviewModal />} />
-          <Route path="/PhotoReviewModal" element={<PhotoReviewModal />} />
-          <Route path="/ForumModal" element={<ForumModal />} />
-          <Route path="/KakaoMap" element={<KakaoMap />} />
+
           <Route path="/Store" element={<Store />} />
           <Route path="/Store/event/*" element={<EventPage />} />
           <Route path="/Store/over" element={<EventOver />} />
--- src/components/MyPage.jsx
+++ src/components/MyPage.jsx
@@ -1,12 +1,39 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import axios from 'axios'
 
 import image1 from '../images/image1.jpg';
 import edit from '../images/edit.svg';
 
-const MyPage = () => {
+// const userId = localStorage.getItem('userId');
+// console.log(userId);
 
+
+const MyPage =  () => {
+
+    // const [userInfo, setUserInfo] = useState({
+    //     email: '',
+    //     name: ''
+    // });
+
+    // useEffect(() => {
+    //     const userId = localStorage.getItem('userId');
+    //     console.log(userId);
+    //     if (userId) {
+    //         axios.get(`${process.env.REACT_APP_API_URL}/user/${userId}`)
+    //         .then(response => {
+    //             const { email, nickname } = response.data;
+    //             console.log("Received data:", response.data);
+    //             setUserInfo({ email, name: nickname });
+    //         })
+    //         .catch(error => {
+    //             console.error('Error fetching user info:', error);
+    //         });
+    //     } else {
+    //         console.error('No user ID found');
+    //     }
+    // }, []);
     
     return (
         <div>
@@ -17,68 +44,61 @@ const MyPage = () => {
                     <div className='w-[150px] h-[60px] text-[20px] flex justify-center items-center border-2 rounded-xl cursor-pointer hover:bg-[#365a31] hover:text-white'>내 포럼</div>   
                   </div>
 
-        <div className="flex justify-between">
-          <div className="bg-gradient-to-b from-[#D6EFD8] to-white w-[1040px] h-[840px] rounded-tr-lg">
-            <div className="flex flex-col gap-[40px]">
-              <div className="bg-white w-[970px] h-[225px] mt-[40px] ml-[30px]">
-                <div className="flex gap-[20px]">
-                  <div className="w-[300px] h-[200px] mt-[40px] ml-[40px] text-[20px] space-y-[10px]">
-                    <p>카카오로고</p>
-                    <p>이메일</p>
-                    <p>이름</p>
-                    <p>전화번호</p>
-                  </div>
-                  <div className="w-[300px] h-[200px] mt-[40px] text-[20px] space-y-[10px]">
-                    <p>null</p>
-                    <p>elice@kakao.com</p>
-                    <p>엘리스</p>
-                    <p>010-0000-0000</p>
-                  </div>
-                </div>
-              </div>
-              <div className="bg-white w-[970px] h-[400px] ml-[30px]">
-                <p className="text-[#365a31] font-bold text-[20px] mt-[40px] ml-[40px] mb-[40px]">
-                  최근 포인트 내역
-                </p>
-                <div className="w-[900px] h-[255px] ml-[40px] flex">
-                  <div className="w-[300px] flex flex-col gap-4">
-                    <div className="font-bold border-b-2 border-[#365a31] pb-2">
-                      활동
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <p>리뷰</p>
-                      <p>응모</p>
-                      <p>포럼</p>
-                      <p>포럼</p>
-                      <p>포럼</p>
-                    </div>
-                  </div>
+                    <div className='flex justify-between'>
+                        <div className='bg-gradient-to-b from-[#D6EFD8] to-white w-[1040px] h-[840px] rounded-xl'>
+                            <div className='flex flex-col gap-[40px]'>
+                                <div className='bg-white w-[970px] h-[225px] mt-[40px] ml-[30px]'>
+                                    <div className='flex gap-[20px]'>
+                                        <div className='w-[300px] h-[200px] mt-[40px] ml-[40px] text-[20px] space-y-[10px]'>
+                                            <p>카카오로고</p>
+                                            <p>이메일</p>
+                                            <p>이름</p>
+                                            <p>전화번호</p>
+                                        </div>
+                                        <div className='w-[300px] h-[200px] mt-[40px] text-[20px] space-y-[10px]'>
+                                            <p>null</p>
+                                            <p>elice@kakao.com</p>
+                                            <p>엘리스</p>
+                                            <p>010-0000-0000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='bg-white w-[970px] h-[400px] ml-[30px]'>
+                                    <p className='text-[#365a31] font-bold text-[20px] mt-[40px] ml-[40px] mb-[40px]'>최근 포인트 내역</p>
+                                    <div className='w-[900px] h-[255px] ml-[40px] flex'>
+                                        <div className='w-[300px] flex flex-col gap-4'>
+                                            <div className='font-bold border-b-2 border-[#365a31] pb-2'>활동</div>
+                                            <div className='flex flex-col gap-2'>
+                                                <p>리뷰</p>
+                                                <p>응모</p>
+                                                <p>포럼</p>
+                                                <p>포럼</p>
+                                                <p>포럼</p>
+                                            </div>
+                                        </div>
 
-                  <div className="w-[300px] flex flex-col gap-4">
-                    <div className="font-bold border-b-2 border-[#365a31] pb-2">
-                      상세 내용
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <p>스타벅스 광화문점</p>
-                      <p>달콤 초콜릿 추첨 이벤트</p>
-                      <p>스타벅스 광화문점</p>
-                      <p>스타벅스 광화문점</p>
-                      <p>스타벅스 광화문점</p>
-                    </div>
-                  </div>
+                                        <div className='w-[300px] flex flex-col gap-4'>
+                                            <div className='font-bold border-b-2 border-[#365a31] pb-2'>상세 내용</div>
+                                            <div className='flex flex-col gap-2'>
+                                                <p>스타벅스 광화문점</p>
+                                                <p>달콤 초콜릿 추첨 이벤트</p>
+                                                <p>스타벅스 광화문점</p>
+                                                <p>스타벅스 광화문점</p>
+                                                <p>스타벅스 광화문점</p>
+                                            </div>
+                                        </div>
 
-                  <div className="flex flex-col gap-4 w-[300px]">
-                    <div className="font-bold border-b-2 border-[#365a31] pb-2">
-                      날짜
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <p>2024-07-26</p>
-                      <p>2024-07-27</p>
-                      <p>2024-07-28</p>
-                      <p>2024-07-29</p>
-                      <p>2024-07-30</p>
-                    </div>
-                  </div>
+                                
+                                        <div className='flex flex-col gap-4 w-[300px]'>
+                                            <div className='font-bold border-b-2 border-[#365a31] pb-2'>날짜</div>
+                                            <div className='flex flex-col gap-2'>
+                                                <p>2024-07-26</p>
+                                                <p>2024-07-27</p>
+                                                <p>2024-07-28</p>
+                                                <p>2024-07-29</p>
+                                                <p>2024-07-30</p>
+                                            </div>
+                                        </div>
 
                                 
                                         <div className='flex flex-col gap-4 w-[300px]'>
@@ -129,65 +149,9 @@ const MyPage = () => {
                             </div>
                         </div>
                     </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col gap-[20px]">
-            <div className="flex flex-col w-[300px] rounded-xl border-2 h-[420px] items-center">
-              <img
-                src={image1}
-                alt="image 1"
-                className="w-[250px] h-[250px] mt-[10px] rounded-[125px]"
-              />
-              <div className="flex flex-col items-center mt-[10px]">
-                <div className="flex items-center justify-center space-x-[30px]">
-                  <p className="text-[25px]">닉네임</p>
-                  <img
-                    src={edit}
-                    alt="edit image"
-                    className="w-[30px] h-[30px] cursor-pointer"
-                  />
-                </div>
-                <div className="flex flex-col items-center mt-[20px]">
-                  <div className="flex justify-between w-full px-[10px] gap-[50px]">
-                    <p className="text-blue-500 font-bold">나의 포인트</p>
-                    <p>500</p>
-                  </div>
-                  <div className="flex justify-between w-full px-[10px] mt-[5px]">
-                    <p className="text-blue-500 font-bold">응모건수</p>
-                    <p>10</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="rounded-xl border-2 w-[300px] h-[400px] flex">
-              <div className="mt-[40px] ml-[40px]">
-                <p className="text-[20px]">당첨 결과 공지</p>
-                <div className="mt-[40px] flex flex-col gap-[10px]">
-                  <p className="text-gray-500 text-[20px]">
-                    이런이벤트 당첨자 결과
-                  </p>
-                  <p className="text-gray-500 text-[20px]">
-                    이런이벤트 당첨자 결과
-                  </p>
-                  <p className="text-gray-500 text-[20px]">
-                    이런이벤트 당첨자 결과
-                  </p>
-                  <p className="text-gray-500 text-[20px]">
-                    이런이벤트 당첨자 결과
-                  </p>
-                  <p className="text-gray-500 text-[20px]">
-                    이런이벤트 당첨자 결과
-                  </p>
-                </div>
-              </div>
             </div>
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
 export default MyPage;
