456cc37654b49821594111f81a1da2450bd81711
somgyul <namhee9096@gmail.com>
2024-08-09 00:03:30 +0900

    FooterPages update

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';  // Link 컴포넌트 추가
 import './Footer.css';
 
 const Footer = () => {
@@ -8,8 +9,8 @@ const Footer = () => {
                 <div className="footer-company">
                     <p><span className="bold">㈜Lucky-Vicky</span></p>
                     <p><span className="bold">대표이사: </span>이현호</p>
-                    <p><span className="bold">주소: </span>나영민지시 우현구 민관로 433, 솜귤 6층
-                    <p></p><span className="bold">사업자 등록 번호:</span> 144-81-00000</p>
+                    <p><span className="bold">주소: </span>나영민지시 우현구 민관로 433, 솜귤 6층</p>
+                    <p><span className="bold">사업자 등록 번호:</span> 144-81-00000</p>
                     <br></br>
                     <div className="footer-contact">
                         <p><span className="bold">대표 번호:</span> 1566-1234</p>
@@ -20,15 +21,14 @@ const Footer = () => {
                     </div>
                 </div>
                 <div className="footer-bottom">
-                    <div className="footer-copyright">
-                        <div className="bold">© Copyright 2024. eco-friendly-thinking. ALL RIGHTS RESERVED.</div>
-                    </div>
                     <div className="footer-links">
-                        <div className="footer-link">약관·정책</div>
+                        <div className="bold">© Copyright 2024. eco-friendly-thinking. ALL RIGHTS RESERVED.</div>
                         <p className="separator">|</p>
-                        <div className="footer-link">개인정보 처리 방침</div>
+                        <Link to="/Terms" className="footer-link">약관·정책</Link>
                         <p className="separator">|</p>
-                        <div className="footer-link">운영 정책</div>
+                        <Link to="/PrivacyPolicy" className="footer-link">개인정보 처리 방침</Link>
+                        <p className="separator">|</p>
+                        <Link to="/OperatingPolicy" className="footer-link">운영 정책</Link>
                     </div>
                 </div>
             </div>
